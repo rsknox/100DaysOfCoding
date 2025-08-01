ea3897c3ac4a85e649c97f3acc6d84b073bedc79
R.S <rsknox@gmail.com>
2025-08-01 16:41:07 -0500

    Updated hangman

--- Hangman.py
+++ Hangman.py
@@ -1,7 +1,13 @@
 import random
+
+from MaxNrInList import length
+
 word_list = ["aardvark", "baboon", "camel"]
 i = random.randint(0,2)
 secret_word = word_list[i]
+secret_word_string = str(secret_word)
 print(f"secret word is: ",{word_list[i]})
-for letters in secret_word:
+for letters in secret_word_string:
     print(letters)
+length_of_secret_word = len(word_list[i])
+print(f"Length of secret word: {length_of_secret_word}")
