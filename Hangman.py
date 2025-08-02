dca7e404fe3b19e9c26662f9f287d510fde83194
Randall S. Knox <rsknox@users.noreply.github.com>
2025-08-02 16:29:39 -0500

    updated hangman

--- Hangman.py
+++ Hangman.py
@@ -3,6 +3,7 @@ import random
 from MaxNrInList import length
 
 word_list = ["aardvark", "baboon", "camel"]
+letter_tally = []
 i = random.randint(0,2)
 secret_word = word_list[i]
 secret_word_string = str(secret_word)
@@ -11,3 +12,10 @@ for letters in secret_word_string:
     print(letters)
 length_of_secret_word = len(word_list[i])
 print(f"Length of secret word: {length_of_secret_word}")
+guess = input("Guess a letter: ")
+for letters in secret_word_string:
+    if guess == letters:
+        letter_tally.append(guess)
+    else:
+        letter_tally.append("_")
+    print(letter_tally)
